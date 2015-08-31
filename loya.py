94c8204120f991833bc89ebeb67590732b3fd94c
sn-work <sn@kchgta.ru>
2015-08-31 18:05:09 +0300

    111

--- loya.py
+++ loya.py
@@ -24,21 +24,29 @@ def main():
 
   db = MySQLdb.connect(db = dbname,passwd = dbpass,host = dbhost, user = dbuser, charset='utf8')
   c = db.cursor()
-  sql = """SELECT users.id,users_accounts.id,users.full_name,balance,users.mobile_telephone,additional_params.lastSend,additional_params.sendOff
-        FROM accounts LEFT JOIN blocks_info ON accounts.block_id = blocks_info.id,
+
+  sql = """SELECT users.id,users_accounts.id,users.full_name,balance,users.mobile_telephone,additional_params.lstart,min((TO_DAYS(curdate()) - TO_DAYS(FROM_UNIXTIME(bi.start_date))) DIV 30) as month_loyalty FROM
+        accounts LEFT JOIN blocks_info AS bi ON accounts.id = bi.account_id,
         users_accounts,
-        users LEFT JOIN (SELECT userid AS uid,(SELECT CONCAT(id,';',value) FROM user_additional_params
-            WHERE userid = uid AND paramid = (SELECT paramid FROM uaddparams_desc WHERE name = 'sms_sended')) AS lastSend,
-            (SELECT value FROM user_additional_params WHERE userid = uid AND paramid = (SELECT paramid FROM uaddparams_desc WHERE name = 'sms_off')) AS sendOff
-            FROM user_additional_params GROUP BY uid)
-             AS additional_params ON additional_params.uid = users.id
+        users LEFT JOIN
+         (SELECT userid AS uid, UNIX_TIMESTAMP(str_to_date(value,'%d/%m/%Y')) AS lstart FROM user_additional_params
+          WHERE paramid = (SELECT paramid FROM uaddparams_desc WHERE name = 'loyalty_start')
+          GROUP BY uid) AS additional_params ON additional_params.uid = users.id
         WHERE users_accounts.uid = users.id
+        AND bi.start_date >= additional_params.lstart
+        AND block_type = 1
         AND users_accounts.account_id = accounts.id
         AND NOT accounts.is_deleted
         AND NOT unlimited
         AND LENGTH(TRIM(mobile_telephone))
-        AND (blocks_info.is_deleted IS NULL OR blocks_info.is_deleted)
-        AND balance < %s"""
+        AND lstart group by users_accounts.account_id"""
+
+  #Выборка платежей за предыдущие сутки
+  sql = """SELECT account_id, SUM(payment_absolute) as summa, users.id FROM payment_transactions INNER JOIN users ON users.basic_account = payment_transactions.account_id
+        WHERE payment_transactions.actual_date >= UNIX_TIMESTAMP(date_sub(curdate( ), INTERVAL 1 DAY)) AND payment_transactions.actual_date < UNIX_TIMESTAMP(curdate( )) AND
+        payment_transactions.payment_absolute > 0 AND payment_transactions.method IN %s AND
+        users.is_juridical = 0 AND is_canceled = 0 GROUP BY payment_transactions.account_id ORDER BY actual_date DESC"""
+
   c.execute(sql,(minBalanse,))
 
   sent = u''
@@ -76,3 +84,18 @@ def main():
 
 if __name__ == '__main__': main()
 
+sql = """SELECT users.id,users_accounts.id,users.full_name,balance,users.mobile_telephone,additional_params.lastSend,additional_params.sendOff
+        FROM accounts LEFT JOIN blocks_info ON accounts.block_id = blocks_info.id,
+        users_accounts,
+        users LEFT JOIN (SELECT userid AS uid,(SELECT CONCAT(id,';',value) FROM user_additional_params
+            WHERE userid = uid AND paramid = (SELECT paramid FROM uaddparams_desc WHERE name = 'sms_sended')) AS lastSend,
+            (SELECT value FROM user_additional_params WHERE userid = uid AND paramid = (SELECT paramid FROM uaddparams_desc WHERE name = 'sms_off')) AS sendOff
+            FROM user_additional_params GROUP BY uid)
+             AS additional_params ON additional_params.uid = users.id
+        WHERE users_accounts.uid = users.id
+        AND users_accounts.account_id = accounts.id
+        AND NOT accounts.is_deleted
+        AND NOT unlimited
+        AND LENGTH(TRIM(mobile_telephone))
+        AND (blocks_info.is_deleted IS NULL OR blocks_info.is_deleted)
+        AND balance < %s"""
